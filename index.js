7c063b624546078dccee9d6701e548aac2d2d8c3
Jessie A <jasherworks@gmail.com>
2026-08-01 18:04:14 -0400

    now if we console log this we're going to get every object turned into html

--- index.js
+++ index.js
@@ -3,9 +3,16 @@
 async function main() { 
  const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b")
  const moviesData = await movies.json();
- console.log(moviesData);
- moviesData.map(movie => )
- const x = 
+ console.log(
+ moviesData.map(
+    (movie) => `<div class="movie-card">
+    <img src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_QL75_UX380_CR0,1,380,562_.jpg" alt="Card 1">
+    <h2>Movie Five Title</h2>
+    <p><b>Series:</b>Series X</p>
+    <p><b>Episode:</b>: Episode X</p>
+  </div>`
+    )
+ );
 } 
 
 main();
