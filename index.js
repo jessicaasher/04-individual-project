e5247d2c394b87dd0f2d867d0eaada3ed135e729
Jessie A <jasherworks@gmail.com>
2026-08-01 17:15:35 -0400

    in order to get the actual javascript we can use write json()

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // API "https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b"
 
-console.log(fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b"))
+console.log(fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b").json())  
