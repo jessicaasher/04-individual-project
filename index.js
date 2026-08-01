9e578ec6fe479f9675844d184fdef555da4c7509
Jessie A <jasherworks@gmail.com>
2026-08-01 18:19:26 -0400

    set the innerHTML to this map

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ async function main() {
  const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b")
  const moviesData = await movies.json();
  const movieListEl = document.querySelector(".movie-list");
- moviesData
+ 
+ movieListEl.innerHTML = moviesData
     .map(
         (movie) => `<div class="movie-card">
         <img src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_QL75_UX380_CR0,1,380,562_.jpg" alt="Card 1">
