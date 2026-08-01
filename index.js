0b4fec4c351812b946cc9d86b6fe31e5428380e0
Jessie A <jasherworks@gmail.com>
2026-08-01 17:47:07 -0400

    but we don't want an array full of objects, we want an array full of html elements

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 async function main() { 
  const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b")
  const movieData = movies.json();
- console.log(await movieData);
+ const x = `<div class="movie-card">
+    <img src="https://via.placeholder.com/250x150" alt="Card 1">
+    <h2>Movie Five Title</h2>
+    <p><b>Series:</b>Series X</p>
+    <p><b>Episode:</b>: Episode X</p>
+  </div>`
 } 
 
 main();
