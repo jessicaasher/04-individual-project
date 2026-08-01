174ce2fad780446443a7c31c069468f6fd22ba05
Jessie A <jasherworks@gmail.com>
2026-08-01 17:53:39 -0400

    we need to convert every single object in this moviesData to html

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 async function main() { 
  const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b")
- const movieData = movies.json();
+ const moviesData = await movies.json();
+
  const x = `<div class="movie-card">
-    <img src="https://via.placeholder.com/250x150" alt="Card 1">
+    <img src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_QL75_UX380_CR0,1,380,562_.jpg" alt="Card 1">
     <h2>Movie Five Title</h2>
     <p><b>Series:</b>Series X</p>
     <p><b>Episode:</b>: Episode X</p>
