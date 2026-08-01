0437f3a2bbc996396ef6d34ced2c33eadf8aacc8
Jessie A <jasherworks@gmail.com>
2026-08-01 17:28:47 -0400

    lets fetch the users

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // API "https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b"
 
-async function main() {
- const users
- console.log(await (await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b").json()))
+async function main() { 
+ const users = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b")
+ console.log(await ().json());
 } 
 
 main();
