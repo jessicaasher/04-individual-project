b3471477198d763f096e1757c98f9f05568bf125
Jessie A <jasherworks@gmail.com>
2026-08-02 17:03:14 -0400

    access the movie object that we're on

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ async function main() {
  const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b")
  const moviesData = await movies.json();
  const movieListEl = document.querySelector(".movie-list");
- 
+ console.log(moviesData);
  movieListEl.innerHTML = moviesData
     .map(
-        (movie) => `<div class="movie-card">
+        (movie) => 
+            `<div class="movie-card">
         <img src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_QL75_UX380_CR0,1,380,562_.jpg" alt="Card 1">
-        <h2>Movie Five Title</h2>
-        <p><b>Series:</b>Series X</p>
-        <p><b>Episode:</b>: Episode X</p>
+        <h2>${movie.Title}</h2>
+        <p><b>Year:</b> ${movie.Year}</p>
+        <p><b>Runtime:</b> ${movie.Runtime}</p>
     </div>`
-        )
-        .join("")
- );
+    )
+    .join("");
 } 
 
 main();
