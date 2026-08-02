fb9a8c86f5d587e5801e0070f5e5933033e752e4
Jessie A <jasherworks@gmail.com>
2026-08-02 17:12:38 -0400

    move this to the top

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // API "https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b"
+const movieListEl = document.querySelector(".movie-list");
 
 async function main() { 
  const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b")
  const moviesData = await movies.json();
- const movieListEl = document.querySelector(".movie-list");
  console.log(moviesData);
  movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("");
 } 
