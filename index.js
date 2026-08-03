c00485a9d7866e164ba5ccfa030d8800ef855c8f
Jessie A <jasherworks@gmail.com>
2026-08-03 17:37:18 -0400

    add a click listener in our html element

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // API "https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b"
+// API "https://www.omdbapi.com/?s=fast&&apikey=a0269a8b"
+
 const movieListEl = document.querySelector(".movie-list");
 
 async function main() { 
- const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a0269a8b")
+ const movies = await fetch("https://www.omdbapi.com/?s=fast&&apikey=a0269a8b")
  const moviesData = await movies.json();
  console.log(moviesData);
  movieListEl.innerHTML = moviesHTML(moviesData);
@@ -10,7 +12,7 @@ async function main() {
 main();
 
 function movieHTML(movie) {
-    return `<div class="movie-card">
+    return `<div class="movie-card" onclick="showMovieDetails(movie)">
         <img src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_QL75_UX380_CR0,1,380,562_.jpg" alt="Card 1">
         <h2>${movie.Title}</h2>
             <p><b>Year:</b> ${movie.Year}</p>
